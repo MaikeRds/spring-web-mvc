e3705ae92a7c216f80735b664d3b0dd59e47c275
Maike Rodrigues <maikedev7@gmail.com>
2021-09-19 16:50:18 -0300

    Iniciando terinamento intermediario

---
+++ spring-web-mvc-intermediario/src/test/java/com/example/springwebmvcintermediario/SpringWebMvcIntermediarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebmvcintermediario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcIntermediarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
