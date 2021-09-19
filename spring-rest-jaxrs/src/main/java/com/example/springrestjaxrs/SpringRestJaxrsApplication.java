b978b4e5f99a949f66e02f9947cdf70337720ca6
Maike Rodrigues <maikedev7@gmail.com>
2021-09-19 17:02:54 -0300

    Projeto adicionado

---
+++ spring-rest-jaxrs/src/main/java/com/example/springrestjaxrs/SpringRestJaxrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestjaxrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestJaxrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestJaxrsApplication.class, args);
+	}
+
+}
---
+++ spring-rest-jaxrs/src/test/java/com/example/springrestjaxrs/SpringRestJaxrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestjaxrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestJaxrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
